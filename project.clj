e5eaa1dd8e0c7bc4bd3967eb8d3e94f82f3a25c8
tom <tom@the-cloud>
2021-04-08 16:41:48 +0000

    Bumped deps

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject stoke "0.1.0-SNAPSHOT"
+(defproject stoke "0.1.1-SNAPSHOT"
   :description "Framework for Portfolio Analysis of Stochastic Demands"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [spork "0.2.1.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [spork "0.2.1.4-SNAPSHOT"]
                  [marathon-schemas "4.1.6-SNAPSHOT"]])
